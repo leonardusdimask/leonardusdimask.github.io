dd77f17d1a2a0fee76ae42cf65f9ff32c5ecfa4f
Leonardus Dimas Klayan Gusti <klayskandal@gmail.com>
2021-05-25 17:22:11 +0700

    commit

--- forms/index.php
+++ forms/index.php
@@ -1,17 +1,33 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["subject"];
-	$content = $_POST["content"];
-
-	$conn = mysqli_connect("localhost", "root", "test", "blog_samples") or die("Connection Error: " . mysqli_error($conn));
-	mysqli_query($conn, "INSERT INTO tblcontact (user_name, user_email,subject,content) VALUES ('" . $name. "', '" . $email. "','" . $subject. "','" . $content. "')");
-	$insert_id = mysqli_insert_id($conn);
-	//if(!empty($insert_id)) {
-	   $message = "Your contact information is saved successfully.";
-	   $type = "success";
-	//}
-}
-require_once "contact-view.php";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+     
+    $EmailTo = "dimascahls@gmail.com"; 
+    $Subject = "Portfolio CV/Resume";
+     
+    // prepare email body text
+    
+    $Body .= "Name: ";
+    $Body .= $name;
+    $Body .= "\n"; 
+     
+    $Body .= "Email: ";
+    $Body .= $email;
+    $Body .= "\n";
+     
+    $Body .= "Message: ";
+    $Body .= $message;
+    $Body .= "\n";
+     
+    
+    // send email
+    $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+     
+    // redirect to success page
+    if ($success){
+       echo "success";
+    }else{
+        echo "invalid";
+    } 
 ?>
