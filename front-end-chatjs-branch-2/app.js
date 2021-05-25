f07ae1f33816b47958addf2c3ce0b18fc66d3a8b
Leonardus Dimas Klayan Gusti <klayskandal@gmail.com>
2021-05-25 20:43:45 +0700

    commit

--- front-end-chatjs-branch-2/app.js
+++
@@ -1,9 +0,0 @@
-const chatButton = document.querySelector('.chatbox__button');
-const chatContent = document.querySelector('.chatbox__support');
-const icons = {
-  isClicked: '</p>Clicked!</p>',
-  isNotClicked: '<p>Not clicked!</p>',
-};
-const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
-chatbox.display();
-chatbox.toggleIcon(false, chatButton);
